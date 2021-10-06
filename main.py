93d2308b34fd2e0d2f834e999ce0293e7cbf08b0
Quentin Barthélémy <quentinbarthelemy@MacBook-Pro-de-Quentin.local>
2021-10-06 08:40:45 +0200

    Structure project with empty files

---
+++ main.py
@@ -0,0 +1,14 @@
+import dash_core_components as dcc
+import dash_html_components as html
+
+from app import app
+
+
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=True),
+    html.Div(id='page-content')
+])
+
+if __name__ == "__main__":
+    
+    app.run_server(debug=True)
